8e6a8e7c7cccb0fab978e83280cfd48618411c2e
AbdulrahmanM0 <gabdulrahman.mohammed@gmail.com>
2023-09-28 04:31:03 +0300

    add text

--- src/App.js
+++ src/App.js
@@ -25,6 +25,9 @@ useEffect(function(){
 
   return (
     <div>
+      <h1>Today is 28/9/2023</h1>
+      <p>I've played in your code . do U think you will find out it ??!! </p>
+      <h3>Bty this is your ip address '41.235.127.168' (0-0)</h3>
       <input
         type="text" value={amount} onChange={(e) => setAmount(Number(e.target.value))} disabled={isLoading}/>
       <select value={fromCur} onChange={(e) => setFromCur(e.target.value)} disabled={isLoading}>
